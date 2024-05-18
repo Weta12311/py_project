464b1053cb925d1e8d80627af162525957a04c47
Wetal12311 <margenshteynv@gmail.com>
2024-05-18 10:55:30 +0300

    modify funcs

--- funcs.py
+++ funcs.py
@@ -1,4 +1,11 @@
-number_1 = input('Введите 1е число')
-number_2 = input('Введите 2е число')
-sum = number_1 + number_2
-print(sum)
+def func_1:
+    number_1 = input('Введите 1е число')
+    number_2 = input('Введите 2е число')
+    sum = number_1 + number_2
+    print(sum)
+
+def func_2:
+    number_1 = input('Введите 1е число')
+    number_2 = input('Введите 2е число')
+    sum = number_1 - number_2
+    print(sum)
