f9fb804fbf81404a48fdcc4ca903cb60c3aa43d7
Wetal12311 <margenshteynv@gmail.com>
2024-05-18 11:06:00 +0300

    исправил функции

--- funcs.py
+++ funcs.py
@@ -1,11 +1,11 @@
-def func_1:
+def func_1():
     number_1 = input('Введите 1е число')
     number_2 = input('Введите 2е число')
-    sum = number_1 + number_2
+    sum = int(number_1) + int(number_2)
     print(sum)
 
-def func_2:
+def func_2():
     number_1 = input('Введите 1е число')
     number_2 = input('Введите 2е число')
-    sum = number_1 - number_2
+    sum = int(number_1) - int(number_2)
     print(sum)
